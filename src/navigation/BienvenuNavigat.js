0bddd1721274f0385066566477126827a54b6910
sabirlkhaloufi <sabirkhaloufi@gmail.com>
2023-10-14 21:14:20 +0100

    add structure and assets

---
+++ src/navigation/BienvenuNavigat.js
@@ -0,0 +1,19 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const BienvenuNavigat = () => {
+    return (
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="Home" component={HomeScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      );
+}
+
+export default BienvenuNavigat
+
